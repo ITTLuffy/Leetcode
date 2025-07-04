0038efa102d586d15a67cace851a52fa67ec75ab
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2025-07-04 17:38:45 +0200

    Feat: implement removeElement method to filter out specified value from array

--- Remove element/rimuovi.java
+++ Remove element/rimuovi.java
@@ -1,3 +1,12 @@
 public class rimuovi {
-    
+        public int removeElement(int[] nums, int val) {
+        int k = 0; // Posizione dove inserire il prossimo numero valido
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
 }
